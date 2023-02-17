e8a3422b9e48c839fb9eaa7b53e4a2aa08785151
Anton Nossov <a.nosov@indev.expert>
2023-02-17 02:10:56 -0800

    .

--- task01/main.cpp
+++ task01/main.cpp
@@ -1,14 +1,9 @@
 #include "../ext/pch.h"
+#include "lib.h"
 
-int main (int argc, char **argv)
-{
-    UNUSED(argc);
-    UNUSED(argv);
+int main (int, char **) {
+    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 
-    // если оставить замену N, как по условию, то не очень хорошо, потому что N не похоже на шаблон
-    // а вот {BUILD_NUMBER} уже лучше похоже
-    std::cout << "build {BUILD_NUMBER}\n"
-                 "Hello, World!" << std::endl;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
